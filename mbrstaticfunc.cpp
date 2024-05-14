06a75e0e9381b49369ae58672199bfd0c89058c1
wesclicdev <wesclicdev@gmail.com>
2024-05-14 15:09:02 +0700

    membuat tampilan untuk id dan nama untuk ditampilkan

--- mbrstaticfunc.cpp
+++ mbrstaticfunc.cpp
@@ -22,3 +22,17 @@ class mahasiswa {
                 setID();
             }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID()
+{
+    id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
