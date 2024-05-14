37c3c965ccdf37c6d20da6fc048fe3e5eadf0130
wesclicdev <wesclicdev@gmail.com>
2024-05-14 16:52:54 +0700

    membuat chain function memanggil tanpa chain

--- destructor.cpp
+++ destructor.cpp
@@ -35,9 +35,9 @@ void angka::isiData(){
 }
 
 int main() {
-    angka belajarcpp(3);
-    angka *ptrBelajarcpp = new angka(5);
-    delete ptrBelajarcpp;
+    angka belajarcpp(3); // constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5); // constructor dipanggil
+    delete ptrBelajarcpp; // Destructor dipanggil
 
     return 0;
-}
+} //Destructor dipanggil
