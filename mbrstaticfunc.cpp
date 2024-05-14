c5b80e07cafecf18f4827c4c7b6091a0004d9d3b
wesclicdev <wesclicdev@gmail.com>
2024-05-14 15:02:12 +0700

    membuat logic isi nama mahasiswa

--- mbrstaticfunc.cpp
+++ mbrstaticfunc.cpp
@@ -12,4 +12,13 @@ class mahasiswa {
 
             void setID();
             void printAll();
-}
+
+            static void setNim(int pNim) {nim = pNim;}
+            static int getNim() {return nim;}
+
+            mahasiswa(string pnama) 
+            {
+                nama = pnama;
+                setID();
+            }
+};
