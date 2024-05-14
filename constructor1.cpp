7b301e39f5fb0b6d436503ab327e91f8d88cd2ad
wesclicdev <wesclicdev@gmail.com>
2024-05-14 14:30:40 +0700

    membuat class mahasiswa dan mendekalarasikan variable nya

--- constructor1.cpp
+++ constructor1.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+    public:
+        static int nim;
+        int id;
+        string nama;
+
+        void setID();
+
+        void printAll();
+}
