1c003f4e39f112f36cc78da3aaa99d8725c20b8e
wesclicdev <wesclicdev@gmail.com>
2024-05-14 14:43:05 +0700

    menjalankan setID dan printAll

--- constructor1.cpp
+++ constructor1.cpp
@@ -20,3 +20,12 @@ class mahasiswa {
 
 int mahasiswa::nim = 237;
 
+void mahasiswa::setID() {
+    id = ++nim;
+}
+
+void mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
